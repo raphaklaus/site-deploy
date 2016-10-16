8359493d51df7827aec05c479555ab2df0080c8d
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-10-16 20:10:48 -0200

    feat: that's pretty much all

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+  app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen('3000', () => {
+  console.log('Connected');
+});
